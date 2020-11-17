c21c3f6b4efdc101e70150c9add273df1712b67e
Matan Budimir <36965687+MatanBudimir@users.noreply.github.com>
2020-11-17 21:38:06 +0100

    Finish role assignment loop

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -1,6 +1,12 @@
 from discord.ext import commands, tasks
 import discord
-from helpers.config import VATSIM_MEMBER_ROLE, CHECK_MEMBERS_INTERVAL
+from helpers.config import VATSIM_MEMBER_ROLE, CHECK_MEMBERS_INTERVAL, VATSCA_MEMBER_ROLE
+import re
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
 
 class TasksCog(commands.Cog):
 
@@ -14,15 +20,37 @@ class TasksCog(commands.Cog):
     def cog_unload(self):
         self.checkMembers.cancel()
 
-    @tasks.loop(seconds=CHECK_MEMBERS_INTERVAL)
+    @tasks.loop(seconds=5)
     async def checkMembers(self):
         guild = self.bot.get_guild(776110954437148672)
         users = guild.members
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            database=os.getenv('DATABASE')
+        )
+
+        cursor = mydb.cursor()
+
         for user in users:
             if discord.utils.get(guild.roles, name=VATSIM_MEMBER_ROLE) not in user.roles:
                 continue
 
-            cid = [int(s) for s in user.nick.split() if s.isdigit()]
+            cid = re.findall('\d+', user.nick)
+            
+            statment = "SELECT subdivision FROM users WHERE id = %s"
+
+            cursor.execute(statment, cid)
+
+            result = cursor.fetchone()
+
+            role = discord.utils.get(guild.roles, id=VATSCA_MEMBER_ROLE)
+
+            if role not in user.roles and result[0] == 'SCA':
+                await user.add_roles(role, reason='Member is now part of VATSCA')
+            elif role in user.roles and result[0] != 'SCA':
+                await user.remove_roles(role, reason='Member is no longer part of VATSCA')
 
 def setup(bot):
     bot.add_cog(TasksCog(bot))
