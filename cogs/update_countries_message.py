7122002c405b3ee74972c7e23e4f0eeb3525dbee
Matan Budimir <matanbudimir@gmail.com>
2021-01-01 23:05:42 +0100

    Ability to edit countries message added

---
+++ cogs/update_countries_message.py
@@ -0,0 +1,62 @@
+import discord
+from discord.ext import commands
+
+from helpers.config import ROLES_CHANNEL
+from helpers.message import embed
+from helpers.message import roles
+
+
+class UpdateCountryMessage(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='update_countries', hidden=True)
+    @commands.has_any_role(*roles())
+    async def update(self, ctx, *, message_id: int = None):
+        """
+        Function posts updated countries message
+        :param ctx:
+        :param message_id:
+        :return:
+        """
+        channel = discord.utils.get(ctx.message.guild.channels, id=ROLES_CHANNEL)
+        if channel:
+            author = {
+                'name': self.bot.user.name,
+                'url': 'https://vatsim-scandinavia.org',
+                'icon': self.bot.user.avatar_url,
+            }
+            if message_id is None:
+                try:
+                    await ctx.message.delete()
+                    text = self._read_file()
+                    msg = embed(description=text, author=author)
+                    await channel.send(embed=msg)
+                except Exception as e:
+                    print(e)
+            else:
+                message = discord.utils.get(await channel.history(limit=100).flatten(), id=message_id)
+
+                if message:
+                    try:
+                        await ctx.message.delete()
+                        text = self._read_file()
+                        msg = embed(description=text, author=author)
+                        await message.edit(embed=msg)
+                    except Exception as e:
+                        print(e)
+
+    def _read_file(self) -> str:
+        """
+        Function reads and returns welcome and rules message stored in countries.md
+        :return:
+        """
+        file = open('messages/countries.md', mode='r')
+        data = file.read()
+        file.close()
+        return data
+
+
+def setup(bot):
+    bot.add_cog(UpdateCountryMessage(bot))
---
+++ messages/countries.md
@@ -0,0 +1,5 @@
+:flag_dk: Access to Denmark's channels
+:flag_fi: Access to Finland's channels
+:flag_is: Access to Iceland's channels
+:flag_no: Access to Norway's channels
+:flag_se: Access to Sweden's channels
