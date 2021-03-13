60a9e8613469e7d2a2ea7f9f59999fb1b9cde7ec
Daniel <blt950@users.noreply.github.com>
2021-03-13 15:52:57 +0100

    Fixed query

--- db/db.sql
+++ db/db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `vatsca_bot` . `events` (
+CREATE TABLE IF NOT EXISTS `events` (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) NOT NULL,
     img varchar(255) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `vatsca_bot` . `events` (
     start_time DATETIME NOT NULL,
     published BOOLEAN DEFAULT FALSE,
     event_id int UNIQUE NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS `vtc` (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
