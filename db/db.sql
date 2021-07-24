e1eb509acd96ecdb78e5b06c40fdbb7a35560b5d
blt950 <blt950@users.noreply.github.com>
2021-07-24 17:12:55 +0200

    Updated db.sql with new structure
    @Marko259 If you gonna deploy locally, remember to just drop and re-make this event table.

--- db/db.sql
+++ db/db.sql
@@ -1,31 +1,22 @@
 
 /* Initial tables */
-CREATE TABLE IF NOT EXISTS `events` (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL,
-    img varchar(255) NOT NULL,
-    url varchar(600) NOT NULL,
-    description TEXT NOT NULL,
-    start_time DATETIME NOT NULL,
-    published BOOLEAN DEFAULT FALSE,
-    event_id int UNIQUE NOT NULL
-);
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `url` varchar(600) NOT NULL,
+  `description` text NOT NULL,
+  `start_time` datetime NOT NULL,
+  `recurring` varchar(255) DEFAULT NULL,
+  `recurring_interval` smallint(6) DEFAULT NULL,
+  `recurring_end` datetime DEFAULT NULL,
+  `published` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`) USING BTREE
+)
 
 CREATE TABLE IF NOT EXISTS `vtc` (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     position varchar(255) NOT NULL,
     name varchar(255) NOT NULL
 );
-
-/* Table alters for event functionality update */ 
-ALTER TABLE `vatsca_discord`.`events` ADD COLUMN `recurring` VARCHAR(255) NULL DEFAULT NULL COMMENT '' AFTER `start_time`;
-ALTER TABLE `vatsca_discord`.`events` ADD COLUMN `recurring_interval` SMALLINT COMMENT '' AFTER `recurring`;
-ALTER TABLE `vatsca_discord`.`events` ADD COLUMN `recurring_end` DATETIME COMMENT '' AFTER `recurring`;
-ALTER TABLE `vatsca_discord`.`events` CHANGE `published` `published` DATETIME NULL COMMENT '';
-
-UPDATE events SET published = start_time WHERE published = "0000-00-00 00:00:00" and UTC_TIMESTAMP() > start_time;
-UPDATE events SET published = NULL WHERE published = "0000-00-00 00:00:00"
-
-ALTER TABLE `vatsca_discord`.`events` DROP INDEX `event_id`;
-CREATE UNIQUE INDEX `id` ON `vatsca_discord`.`events`(`id`) USING BTREE;
-ALTER TABLE `vatsca_discord`.`events` DROP COLUMN `event_id`;
