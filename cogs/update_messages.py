001cfaa5e77e4a096129dea981e6a63ed073e575
blt950 <blt950@users.noreply.github.com>
2022-06-04 09:33:09 +0200

    Update update_messages.py

--- cogs/update_messages.py
+++ cogs/update_messages.py
@@ -134,7 +134,7 @@ class UpdateCountryMessage(commands.Cog):
             if channel:
                 author = {
                     'name': self.bot.user.name,
-                    'url': DIVISION_URL,
+                    'url': 'https://vatsim-scandinavia.org',
                     'icon': self.bot.user.avatar_url,
                 }
                 if message_id is None:
@@ -168,7 +168,7 @@ class UpdateCountryMessage(commands.Cog):
             if channel:
                 author = {
                     'name': self.bot.user.name,
-                    'url': DIVISION_URL,
+                    'url': 'https://vatsim-scandinavia.org',
                     'icon': self.bot.user.avatar_url,
                 }
                 if message_id is None:
