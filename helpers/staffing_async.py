4e27292ec1afffb1a82cdfc4a68a0cfdd5025400
Marko259 <animationsisnice@gmail.com>
2025-01-27 00:59:24 +0100

    Added support for microsec

--- helpers/staffing_async.py
+++ helpers/staffing_async.py
@@ -341,12 +341,12 @@ class StaffingAsync():
             print(f"Start time not found for event '{title}'.")
             return None, None, None, None
 
-        start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")  # Convert to datetime
+        start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S.%f")  # Convert to datetime
         formatted_start_time = start_time.strftime("%H:%M")
 
         # Handle missing end time (default: 2 hours after start)
         if end_time:
-            end_time = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S").strftime("%H:%M")
+            end_time = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S.%f").strftime("%H:%M")
         else:
             end_time = (start_time + timedelta(hours=2)).strftime("%H:%M")
 
@@ -406,7 +406,6 @@ class StaffingAsync():
                 third_section: DB.select(table='positions', columns=['*'], where=['event', 'type'], value={'event': id, 'type': 3}, amount='all'),
                 fourth_section: DB.select(table='positions', columns=['*'], where=['event', 'type'], value={'event': id, 'type': 4}, amount='all')
             }
-
 
             pos_info = ''
             for section, positions in section_positions.items():
